a9c17bfc3040b18402776bf5daeee6725dc76a83
Ayush Gupta <45939262+Ayush-00@users.noreply.github.com>
2020-11-14 11:43:14 +0530

    Updated to have images in directory

--- separate_train_test.py
+++ separate_train_test.py
@@ -1,6 +1,8 @@
 ## Separates the dataset into train and test in separate folders
 import shutil
 import pickle
+import os
+
 
 with open('train_test_split.txt') as f:
     train_test_split = f.read().splitlines()
@@ -25,21 +27,31 @@ for image in images:
         #Value 1 = training, value 0 = test
         #print('Image ID = ' + str(id) + '\t Train or test = ' + str(train_test_split[int(id)].split()[1]))
         #print('Image ID = ' + str(id) + '\t Train')
-        shutil.copyfile('./images/' +path, './train/' + str(count))
-        train_filenames.append(str(count))
+        try:
+            shutil.copyfile('./images/' +path, './train/' + path)
+        except IOError as io_err:
+            os.makedirs(os.path.dirname('./train/' + path))
+            shutil.copyfile('./images/' +path, './train/' + path)
+
+        train_filenames.append(path)
     else:
         #test
         #print('Image ID = ' + str(id) + '\t Train or test = ' + str(train_test_split[int(id)].split()[1]))
         #print('Image ID = ' + str(id) + '\t Test')
-        shutil.copyfile('./images/' + path, './test/' + str(count))
-        test_filenames.append(str(count))
+        try:
+            shutil.copyfile('./images/' + path, './test/' + path)
+        except IOError as io_err:
+            os.makedirs(os.path.dirname('./test/' + path))
+            shutil.copyfile('./images/' +path, './test/' + path)
+
+        test_filenames.append(path)
 
     count+=1;
     if(count%100 == 0):
         print('Reached %sth image' % str(count))
     #if(count == 10):
-    #    print('10 images moved. Exiting....')
-    #    exit(0);
+    #   print('10 images moved. Exiting....')
+    #   exit(0);
 
 with open('train_filenames.pickle', 'wb') as f:
     pickle.dump(train_filenames, f)
